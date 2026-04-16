8ef55531a59b4401bc54ccb3442903d9b5e4250c
royston allfrey <evariste@hotmail.co.uk>
2026-04-16 21:02:47 +0100

    spot details wip

--- src/js/views/statisticDataView.js
+++ src/js/views/statisticDataView.js
@@ -9,32 +9,35 @@ _generateStatMarkup(result) {
     return !result.date
       ? `
     <H1 ><span>Spot details</span><br></H1>
-    <br><div id= statBox>
-    <div id=openPrice >Day Open:<span>${parseFloat(result.open_price).toFixed(
-      2
-    )}</span></div>
-    <div id=high>Day high:<span>${parseFloat(result.high_price).toFixed(
-      2
-    )}</span></div>
-    <div id=low>Day low:<span>${parseFloat(result.low_price).toFixed(
-      2
-    )}</span></div>
-    <div id=buy>Buy:<span>${parseFloat(result.ask).toFixed(2)}</span></div>
-    <div id=sell>Sell:<span>${parseFloat(result.bid).toFixed(2)}</span></div>
-    <div id=P24K>24 Karat:<span>${(result.price_gram_24k * 31.1034768).toFixed(
-      2
-    )}</span></div>
-    <div id=P22K>22 Karat:<span>${(result.price_gram_22k * 31.1034768).toFixed(
-      2
-    )}</span></div>
-    <div id=P20K>20 Karat:<span>${(result.price_gram_20k * 31.1034768).toFixed(
-      2
-    )}</span></div>
-    <div id=P18K>18 Karat:<span>${(result.price_gram_18k * 31.1034768).toFixed(
-      2
-    )}</span></div>
-    
-    
+    <br><div id=statBox style="display: inline-flex; flex-direction: column;" >
+    <div style="display: flex;">
+    <div id=openPrice >Day Open:</div>
+    <span>${parseFloat(result.open_price).toFixed(2)}</span></div>
+    <div style="display: flex;">
+    <div id=high>Day high:</div>
+    <span>${parseFloat(result.high_price).toFixed(2)}</span></div>
+    <div style="display: flex;">
+    <div id=low>Day low:</div>
+    <span>${parseFloat(result.low_price).toFixed(2)}</span></div>
+    <div style="display: flex;">
+    <div id=buy>Buy:</div>
+    <span>${parseFloat(result.ask).toFixed(2)}</span></div>
+    <div style="display: flex;">
+    <div id=sell>Sell:</div>
+    <span>${parseFloat(result.bid).toFixed(2)}</span></div>
+    <div style="display: flex;">
+    <div id=P24K>24 Karat:</div>
+    <span>${(result.price_gram_24k * 31.1034768).toFixed(2)}</span></div>
+    <div style="display: flex;">
+    <div id=P22K>22 Karat:</div>
+    <span>${(result.price_gram_22k * 31.1034768).toFixed(2)}</span></div>
+    <div style="display: flex;">
+    <div id=P20K>20 Karat:</div>
+    <span>${(result.price_gram_20k * 31.1034768).toFixed(2)}</span></div>
+    <div style="display: flex;">
+    <div id=P18K>18 Karat:</div>
+    <span>${(result.price_gram_18k * 31.1034768).toFixed(2)}</span></div>
+        
     </div>
     `
       : console.log(`
